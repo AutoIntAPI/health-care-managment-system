1ff2bb32ac7e5464286db5995638e2bfbcd6c99d
Youssef Abdulmoneim Sayed <140813950+youssefabdulmoneim@users.noreply.github.com>
2026-07-03 02:44:06 +0300

    Update patient_routes.py

--- services/patient-service/app/routes/patient_routes.py
+++ services/patient-service/app/routes/patient_routes.py
@@ -8,7 +8,7 @@ controller = PatientController()
 def create_patient():
     return controller.create(request)
 
-@patient_bp.route('/<int:patient_id>', methods=['GET'])
+@patient_bp.route('/<int:patients_id>', methods=['GET'])
 def get_patient(patient_id):
     return controller.get_by_id(patient_id)
 
@@ -16,15 +16,15 @@ def get_patient(patient_id):
 def get_all_patients():
     return controller.get_all()
 
-@patient_bp.route('/<int:patient_id>', methods=['PUT'])
+@patient_bp.route('/<int:patients_id>', methods=['PUT'])
 def update_patient(patient_id):
     return controller.update(patient_id, request)
 
-@patient_bp.route('/<int:patient_id>', methods=['DELETE'])
+@patient_bp.route('/<int:patients_id>', methods=['DELETE'])
 def delete_patient(patient_id):
     return controller.delete(patient_id)
 
-@patient_bp.route('/<int:patient_id>/notify', methods=['POST'])
+@patient_bp.route('/<int:patient_id>/notifi', methods=['POST'])
 def notify_patient(patient_id):
     """Endpoint to notify patient (used by other services)"""
     data = request.get_json()
